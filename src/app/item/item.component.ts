4e826c1ae31f978ab63fc47f26efc7e0d554cc95
krosenk729 <krosenk729@gmail.com>
2018-03-27 21:31:22 -0400

    updated functionality

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,26 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
+import { DecimalPipe } from '@angular/common';
 
 @Component({
 	selector: 'app-item',
 	templateUrl: './item.component.html',
 	styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
-	units: object[] = [
-		{'value': 'g', 'name': 'grams (g)', 'default': true},
-		{'value': 'oz', 'name': 'ounces (oz)'},
-		{'value': 'each', 'name': 'units (items)'},
-		{'value': 'lbs', 'name': 'pounds (lbs)'}
-	];
+export class ItemComponent {
 	name: string;
-	price: number = 0;
-	sizeqty: number = 1;
-	unit: string = 'g';
+	price: number;
+	qty: number;
+	unit: string;
+	calcQty: number;
+	calcPrice: number;
+	calcUnit: string;
+	units = [
+		{
+			'value': 'g', 
+			'name': 'grams (g)'
+		},
+		{
+			'value': 'oz', 
+			'name': 'ounces (oz)'
+		},
+		{
+			'value': 'each', 
+			'name': 'units (items)'
+		},
+		{
+			'value': 'lbs', 
+			'name': 'pounds (lbs)'
+		}
+	];
+
 	constructor() {
+		this.price = 0;
+		this.qty = 1;
+		this.unit = 'g';
+		this.calcQty = this.qty;
+		this.calcUnit = this.unit;
+		this.calcPrice = this.price;
 	}
 
-	ngOnInit() {
+	findNewPrice(endUnit, startUnit, price, qty, endqty = 1){
+		console.log('finding new price');
+		let conversionFactor = 1;
+		switch (endUnit) {
+			case 'g':
+				if(startUnit == 'oz') conversionFactor = 0.03527396
+				if(startUnit == 'lbs') conversionFactor = 0.00220462
+				break;
+			case 'oz':
+				if(startUnit == 'g') conversionFactor = 28.35
+				if(startUnit == 'lbs') conversionFactor = 0.0625
+				break;
+			case 'lbs':
+				if(startUnit == 'oz') conversionFactor = 16
+				if(startUnit == 'g') conversionFactor = 453.59237
+				break;
+		}
+		return price * conversionFactor * endqty / qty;
+	}
+
+	handleNewCalc(){
+		this.calcPrice = this.findNewPrice(this.calcUnit, this.unit, this.price, this.qty, this.calcQty);
 	}
 
 }
